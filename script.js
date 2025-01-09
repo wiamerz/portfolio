9355b3e72d460ade22f32f87a5e6adcb7e6ff66f
wiamerz <wiamerm2023@gmail.com>
2025-01-09 16:34:28 +0100

    Portfolio, create home section

---
+++ script.js
@@ -0,0 +1,105 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // $(document).ready(function() {
+        //     // Navigation handling
+        //     $('#homeLink').click(function(e) {
+        //         e.preventDefault();
+        //         $('.container > div').addClass('hidden');
+        //         $('#homeSection').removeClass('hidden');
+        //     });
+
+        //     $('#aboutLink').click(function(e) {
+        //         e.preventDefault();
+        //         $('.container > div').addClass('hidden');
+        //         $('#aboutSection').removeClass('hidden');
+        //     });
+
+        //     $('#contactLink').click(function(e) {
+        //         e.preventDefault();
+        //         $('.container > div').addClass('hidden');
+        //         $('#contactSection').removeClass('hidden');
+        //     });
+
+        //     // Like button functionality
+        //     $('.like-button').click(function() {
+        //         $(this).toggleClass('liked');
+        //         let likeCount = $(this).siblings('.like-count');
+        //         let currentLikes = parseInt(likeCount.text());
+        //         if ($(this).hasClass('liked')) {
+        //             likeCount.text(currentLikes + 1);
+        //         } else {
+        //             likeCount.text(currentLikes - 1);
+        //         }
+        //     });
+
+        //     // Contact form handling
+        //     $('#contactForm').submit(function(e) {
+        //         e.preventDefault();
+        //         let email = $('#email').val();
+        //         let message = $('#message').val();
+        //         alert('Message sent!\nEmail: ' + email + '\nMessage: ' + message);
+        //         this.reset();
+        //     });
+        // });
