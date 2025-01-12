78699aa16b682c8772fa8c748308a9e232dca7c8
wiamerz <wiamerm2023@gmail.com>
2025-01-12 21:25:50 +0100

    contact connection with googlesheet

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function () {
-    $('nav a').on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            const hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
-});
-
-$(document).ready(function () {
-    const text = $('#animated-text').text(); // Get the full text
-    let currentIndex = 0;
-
-    // Clear the text to start the animation
-    $('#animated-text').text('');
-
-    // Function to reveal the text letter by letter
-    function typeLetter() {
-        if (currentIndex < text.length) {
-            $('#animated-text').append(text[currentIndex]); // Add one letter
-            currentIndex++;
-            setTimeout(typeLetter, 150); // Adjust speed (in milliseconds)
-        }
-    }
-
-    typeLetter(); // Start the animation
-});
-
-
-=======
 $(document).ready(function () {
     $('nav a').on('click', function (event) {
         if (this.hash !== "") {
@@ -71,5 +35,3 @@ $(document).ready(function () {
     typeLetter(); // Start the animation
 });
 
-
->>>>>>> 74033ec (projects section, contact section)
